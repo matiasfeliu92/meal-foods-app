c9db00306fb58d6662840fa9c2fd758dcea7c7f9
matiasfeliu92 <matumazparrote@gmail.com>
2023-05-26 18:57:59 -0300

    refactor: update Categories component

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -35,6 +35,7 @@ const Categories = ({categorySelected, onSelect, setCatSelect}: Props) => {
 
   return (
     <div>
+      <h3>select one category</h3>
       <select onChange={handleCategoryChange}>
         {categories.length > 0 &&
           categories.map((category) => (
