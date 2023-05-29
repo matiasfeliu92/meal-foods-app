9845d79b899c3b5c2d44c6f2cdfa0128837b84c1
matiasfeliu92 <matumazparrote@gmail.com>
2023-05-29 18:32:10 -0300

    refactor: update filterByAreas, filterByIngredients, filterByCategories methods in dataService

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -1,4 +1,5 @@
 import ICategories from "../interfaces/categories";
+import IMeal from "../interfaces/meal";
 import api from "./axios";
 
 class DataService {
@@ -14,20 +15,23 @@ class DataService {
         return api.get('/list.php?i=list')
     }
 
-    filterByCategories(category: string) {
+    filterByCategories(category: string): Promise<IMeal[]> {
         console.log(category)
         return api.get(`/filter.php?c=${category}`)
+            .then((response) => response.data.meals);
     }
 
-    filterByIngredients(ingredient: string) {
+    filterByIngredients(ingredient: string): Promise<IMeal[]> {
         console.log(ingredient)
         return api.get(`/filter.php?i=${ingredient}`)
+            .then((response) => response.data.meals);
     }
 
-    filterByAreas(area: string) {
+    filterByAreas(area: string): Promise<IMeal[]> {
         console.log(area)
         return api.get(`/filter.php?a=${area}`)
+            .then((response) => response.data.meals);
     }
 }
 
-export default DataService
+export default DataService;
