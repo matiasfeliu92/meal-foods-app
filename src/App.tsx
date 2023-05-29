81154bb2e96ac24db4f2dd60d7f80d497745e6da
matiasfeliu92 <matumazparrote@gmail.com>
2023-05-29 11:13:57 -0300

    refactor: add GlobalStyle in App.tsx main page component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Categories from "./components/Categories";
 import Ingredients from "./components/Ingredients";
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles/navbar.css";
-import "./styles/header.css";
+import GlobalStyle from "./utils/GlobalStyles";
 
 function App() {
   const [categorySelected, setCategorySelected] = useState("");
@@ -14,33 +14,34 @@ function App() {
   const [areaSelected, setAreaSelected] = useState("");
 
   return (
-    <div style={{backgroundColor: 'lightsalmon', height: "100%"}}>
-      <div className="header">
-        <h1>Meals App</h1>
-      </div>
-      <div className="navbar d-flex justify-content-around">
-        <Categories
-          onSelect={(category: string) => setCategorySelected(category)}
-          categorySelected={categorySelected}
-          setCatSelect={setCategorySelected}
-        />
-        <Ingredients
-          onSelect={(ingredient: string) => setIngredientSelected(ingredient)}
-          selectedIngredient={ingredientSelected}
-          setIngredientSelect={setIngredientSelected}
-        />
-        <Areas
-          onSelect={(area: string) => setAreaSelected(area)}
-          selectedArea={areaSelected}
-          setAreaSelected={setAreaSelected}
-        />
-      </div>
+    <>
+    <GlobalStyle/>
+    <div>
+        <div className="navbar d-flex justify-content-around">
+          <img className="img-thumb" src="https://www.themealdb.com/images/meal-icon.png"/>
+          <Categories
+            onSelect={(category: string) => setCategorySelected(category)}
+            categorySelected={categorySelected}
+            setCatSelect={setCategorySelected}
+          />
+          <Ingredients
+            onSelect={(ingredient: string) => setIngredientSelected(ingredient)}
+            selectedIngredient={ingredientSelected}
+            setIngredientSelect={setIngredientSelected}
+          />
+          <Areas
+            onSelect={(area: string) => setAreaSelected(area)}
+            selectedArea={areaSelected}
+            setAreaSelected={setAreaSelected}
+          />
+        </div>
       <Meals
         categorySelected={categorySelected}
         ingredientSelected={ingredientSelected}
         areaSelected={areaSelected}
       />
     </div>
+    </>
   );
 }
 
