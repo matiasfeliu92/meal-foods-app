66fbbcb86ac004da442fc7287bd94a12f84e6554
matiasfeliu92 <matumazparrote@gmail.com>
2023-05-29 10:17:13 -0300

    refactor: update main page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
-import React, { useState } from 'react';
-import './App.css';
-import Meals from './components/Meals';
-import Areas from './components/Areas';
-import Categories from './components/Categories';
-import Ingredients from './components/Ingredients';
-import ICategories from './interfaces/categories';
+import React, { useState } from "react";
+import "./App.css";
+import Meals from "./components/Meals";
+import Areas from "./components/Areas";
+import Categories from "./components/Categories";
+import Ingredients from "./components/Ingredients";
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles/navbar.css";
+import "./styles/header.css";
 
 function App() {
-  const [categorySelected, setCategorySelected] = useState('')
-  const [ingredientSelected, setIngredientSelected] = useState('')
-  const [areaSelected, setAreaSelected] = useState('')
+  const [categorySelected, setCategorySelected] = useState("");
+  const [ingredientSelected, setIngredientSelected] = useState("");
+  const [areaSelected, setAreaSelected] = useState("");
 
   return (
-    <div className="App">
-      <Categories onSelect={(category: string) => setCategorySelected(category)} categorySelected={categorySelected} setCatSelect={setCategorySelected}/>
-      <Ingredients onSelect={(ingredient:string) => setIngredientSelected(ingredient)} selectedIngredient={ingredientSelected} setIngredientSelect={setIngredientSelected}/>
-      <Areas onSelect={(area:string) => setAreaSelected(area)} selectedArea={areaSelected} setAreaSelected={setAreaSelected}/>
-      <Meals categorySelected={categorySelected} ingredientSelected={ingredientSelected} areaSelected={areaSelected}/>
+    <div style={{backgroundColor: 'lightsalmon', height: "100%"}}>
+      <div className="header">
+        <h1>Meals App</h1>
+      </div>
+      <div className="navbar d-flex justify-content-around">
+        <Categories
+          onSelect={(category: string) => setCategorySelected(category)}
+          categorySelected={categorySelected}
+          setCatSelect={setCategorySelected}
+        />
+        <Ingredients
+          onSelect={(ingredient: string) => setIngredientSelected(ingredient)}
+          selectedIngredient={ingredientSelected}
+          setIngredientSelect={setIngredientSelected}
+        />
+        <Areas
+          onSelect={(area: string) => setAreaSelected(area)}
+          selectedArea={areaSelected}
+          setAreaSelected={setAreaSelected}
+        />
+      </div>
+      <Meals
+        categorySelected={categorySelected}
+        ingredientSelected={ingredientSelected}
+        areaSelected={areaSelected}
+      />
     </div>
   );
 }
