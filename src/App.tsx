7488cda2cf9a6a074ee1fb028fcedeb662f8c6c5
matiasfeliu92 <matumazparrote@gmail.com>
2023-05-29 18:30:49 -0300

    refactor: update states and add SelectData component

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,76 @@
 import React, { useState } from "react";
 import "./App.css";
 import Meals from "./components/Meals";
-import Areas from "./components/Areas";
-import Categories from "./components/Categories";
-import Ingredients from "./components/Ingredients";
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles/navbar.css";
 import GlobalStyle from "./utils/GlobalStyles";
+import SelectData from "./components/SelectData";
+import DataService from "./services/dataService";
+import ICategories from "./interfaces/categories";
+import IIngredients from "./interfaces/ingredients";
+import IAreas from "./interfaces/areas";
+import { AxiosResponse } from "axios";
 
 function App() {
-  const [categorySelected, setCategorySelected] = useState("");
-  const [ingredientSelected, setIngredientSelected] = useState("");
-  const [areaSelected, setAreaSelected] = useState("");
+  const dataService = new DataService();
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedIngredient, setSelectedIngredient] = useState("");
+  const [selectedArea, setSelectedArea] = useState("");
+
+  const [categories, setCategories] = useState<ICategories[]>([]);
+  const [ingredients, setIngredients] = useState<IIngredients[]>([]);
+  const [areas, setAreas] = useState<IAreas[]>([]);
+
+  const handleCategorySelect = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  const handleIngredientSelect = (ingredient: string) => {
+    setSelectedIngredient(ingredient);
+  };
+
+  const handleAreaSelect = (area: string) => {
+    setSelectedArea(area);
+  };
 
   return (
     <>
-    <GlobalStyle/>
-    <div>
+      <GlobalStyle />
+      <div>
         <div className="navbar d-flex justify-content-around">
-          <img className="img-thumb" src="https://www.themealdb.com/images/meal-icon.png"/>
-          <Categories
-            onSelect={(category: string) => setCategorySelected(category)}
-            categorySelected={categorySelected}
-            setCatSelect={setCategorySelected}
+          <img
+            className="img-thumb"
+            src="https://www.themealdb.com/images/meal-icon.png"
           />
-          <Ingredients
-            onSelect={(ingredient: string) => setIngredientSelected(ingredient)}
-            selectedIngredient={ingredientSelected}
-            setIngredientSelect={setIngredientSelected}
+          <SelectData
+            onSelect={handleCategorySelect}
+            selected={selectedCategory}
+            dataService={(value: string) => dataService.filterByCategories(value)}
+            data={categories}
+            placeholder="Select a category"
           />
-          <Areas
-            onSelect={(area: string) => setAreaSelected(area)}
-            selectedArea={areaSelected}
-            setAreaSelected={setAreaSelected}
+          <SelectData
+            onSelect={handleIngredientSelect}
+            selected={selectedIngredient}
+            dataService={(value: string) => dataService.filterByIngredients(value)}
+            data={ingredients}
+            placeholder="Select an ingredient"
+          />
+          <SelectData
+            onSelect={handleAreaSelect}
+            selected={selectedArea}
+            dataService={(value: string) => dataService.filterByAreas(value)}
+            data={areas}
+            placeholder="Select an area"
           />
         </div>
-      <Meals
-        categorySelected={categorySelected}
-        ingredientSelected={ingredientSelected}
-        areaSelected={areaSelected}
-      />
-    </div>
+        <Meals
+          categorySelected={selectedCategory}
+          ingredientSelected={selectedIngredient}
+          areaSelected={selectedArea}
+        />
+      </div>
     </>
   );
 }
